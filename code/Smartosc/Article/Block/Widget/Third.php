65bd8f01039209f65bfd7d23be2383771d05ab5b
Dat <nguyenvandat170296@gmail.com>
2019-10-11 18:45:33 +0700

    seccond

---
+++ code/Smartosc/Article/Block/Widget/Third.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Smartosc\Article\Block\Widget;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+
+class Third extends Template implements BlockInterface
+{
+
+    protected $_template = "widget/thirdblockwidget.phtml";
+
+}
